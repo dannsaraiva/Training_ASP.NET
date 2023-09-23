8e7cab26c1621b2e64db1e3ec7901391880a6e01
Daniel <daniel.saraiva.oliveira@outlook.com>
2023-09-23 14:24:37 -0300

    Reiniciando a construção do projeto.

--- GerenciadorProdutos/GerenciadorProdutos/Models/ProdutoModel.cs
+++ GerenciadorProdutos/GerenciadorProdutos/Models/ProdutoModel.cs
@@ -1,6 +1,28 @@
-﻿namespace GerenciadorProdutos.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace GerenciadorProdutos.Models
 {
     public class ProdutoModel
     {
+        public Int64 Id { get; set; }
+        [Required]
+        public string CodigoDeBarras { get; set; }
+        [Required]
+        public string Descricao { get; set;}
+        public DateTime DataDeValidade { get; set; }
+        public DateTime DataDeRegistro { get; set; }
+        public int Quantidade { get; set; }
+        public decimal? Valor { get; set; }
+        [MaybeNull]
+        public string NomeDaFoto { get; set; }
+        [MaybeNull]
+        public byte[] Foto { get; set; }
+
+        public bool Ativo { get; set; }
+
+
+
+
     }
 }
